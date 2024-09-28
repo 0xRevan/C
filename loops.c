c194e3f669b0c110aefa6a3f8ff89d3cd1641954
Revan <65150515+FerretUDP@users.noreply.github.com>
2024-09-28 13:16:38 +0200

    started at 0 - x++

--- loops.c
+++ loops.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 
+// int main(void) 
+// {
+
+//     int x = 5;
+//     while (x > 0)
+//         {
+//             printf("bark \n");
+//             x --;
+
+//                 // return 0;
+//         }
+// }
+
 int main(void) 
 {
 
-    int x = 5;
-    while (x > 0)
+    int x = 0;
+    while (x < 5)
         {
             printf("bark \n");
-            x --;
+            x ++;
 
                 // return 0;
         }
