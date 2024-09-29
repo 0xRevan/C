7c3037d25be40a91d9d2efe9ab04068022831c7b
Revan <65150515+FerretUDP@users.noreply.github.com>
2024-09-29 14:42:37 +0200

    tackling programming challenges through the use of Mario - printed ???? to the console

--- calculator.c
+++ calculator.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
 
+int add(int f, int g);  // Function prototype
+
 int main(void)
 {
-    // defining x, y 
-int x , y;
+    int x, y;  // Declare x and y
 
-    // get x
+    // Get x
     printf("x? \n");
     scanf("%i", &x);
 
-    // get y
+    // Get y
     printf("y? \n");
     scanf("%i", &y);
 
-    // calculates answer
-        int answer = x + y;
+    // Calculate the answer using the add function
+    int answer = add(x, y);
+    
+    // Print the answer
+    printf("Arithmetics! The answer is: %i\n", answer);
 
-            printf("Arithmetics! The answer is: %i\n", answer);
+    return 0;  // Return 0 to indicate successful execution
+}
 
+// Function to add two integers
+int add(int f, int g)
+{
+    return f + g;  // Return the sum of f and g
 }
---
+++ mario.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// print x out 
+
+int main(void) 
+{
+    for (int x = 0; x < 4; x++)
+    {
+        printf("?");
+    }
+        // removes the $
+        printf("\n");
+}
