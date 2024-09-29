1beb96104122bbc6750c721fe7978366edd444d9
Revan <65150515+FerretUDP@users.noreply.github.com>
2024-09-29 14:51:26 +0200

    a simple grid forming programme ####

---
+++ bricks.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// int main(void) {
+
+//     for (int x = 0; x < 5; x++)
+//     {
+//         printf("####\n");
+//     }
+
+// }
+
+int main(void) {
+
+    for (int x = 0; x < 35; x++)
+    {
+
+        for (int k = 0; k < 15; k++) 
+        {
+            printf("#");
+
+            }
+                printf("\n");
+    }
+
+}
