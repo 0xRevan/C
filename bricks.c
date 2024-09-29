730d6425cfa8943436629433668e40c90d131f49
Revan <65150515+FerretUDP@users.noreply.github.com>
2024-09-29 15:11:51 +0200

    enhanced as per cs50 guidelines. Used do instead

--- bricks.c
+++ bricks.c
@@ -3,18 +3,17 @@
 int main(void) {
 
     int z;  // Declare the variable to hold user input
+do
+
+{
     printf("Size?:\n");
     scanf("%i", &z);  // Read an integer input from the user
-        
-        while (z < 1) {
-            printf("Size?:\n");
-            scanf("%i", &z);  // Read an integer input from the user
-        }
-        
+} 
+while (z < 1);
 
     // Adjust the outer loop based on user input for size
     for (int x = 0; x < z; x++) {
-        for (int k = 0; k < 15; k++) {
+        for (int k = 0; k < 5; k++) {
             printf("#");
         }
         printf("\n");  // Print a newline after each row
