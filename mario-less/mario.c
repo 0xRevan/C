e0df5950a685358005174d3ab5b7d85ed23aa91d
Revan <65150515+FerretUDP@users.noreply.github.com>
2024-10-15 10:34:56 +0200

    initial commit for CS50 problem set 1 "hello, x" exercise works initially, but gives zsh: bus error looking into resolution

---
+++ mario-less/mario.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+// print x out 
+
+int main(void) 
+{
+// gets user input
+    
+    int i;
+    printf("My great Imhotep, what height do you want the pyramid to be?");
+    scanf("%i", &i);
+    printf("Output: %i\n", i);
+
+// use user input to print the ### pyramids
+
+
+
+}
+
